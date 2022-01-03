6e3b2713926d1c689db25989e8223e4adfc7f9b7
sanjadeur <sanja.deur15@gmail.com>
2022-01-03 14:44:45 +0100

    Cover a few edge cases

--- src/photomath.py
+++ src/photomath.py
@@ -9,7 +9,7 @@ from solver import solve
 
 def parse_arguments():
     parser = ArgumentParser(description='Photomath',
-                            epilog='NOTE: Please run util/train.py first if there is no saved model.')
+                            epilog='NOTE: Please run scripts/train.py first in case there is no saved model.')
 
     parser.add_argument('-p', '--photo_path', type=str, required=True,
                         help='path to the photo of a math expression')
@@ -22,6 +22,9 @@ def parse_arguments():
 
     parser.add_argument('--dest', type=str, default='resources/extracted_characters',
                         help='path to the directory where extracted characters will be stored')
+
+    parser.add_argument('--model_path', type=str, default='final_model.h5',
+                        help='path of the trained model')
 
     parser.add_argument('--verbose', type=int, default=1,
                         help='choose 1 for displaying the prediction information, 0 otherwise')
@@ -55,8 +58,16 @@ def detection():
 def classification(photo_name):
     print('\nSECOND PART: Classifying extracted characters...\n')
 
+    if not os.path.exists(args.model_path):
+        sys.stderr.write('ERROR: Path of the trained model does not exist.\n')
+        exit(0)
+
+    if not (args.verbose == 0 or args.verbose == 1):
+        sys.stderr.write('ERROR: Verbose flag should be either 0 or 1.\n')
+        exit(0)
+
     extracted_characters_dir = os.path.join(args.dest, photo_name)
-    math_expression = classify(args.verbose, extracted_characters_dir)
+    math_expression = classify(args.model_path, extracted_characters_dir, args.width, args.height, args.verbose)
 
     print(f'Recognised math expression: "{math_expression}"')
 
@@ -70,7 +81,7 @@ def solution(math_expression):
         result = solve(math_expression)
 
         if result is None:
-            sys.stderr.write('ERROR: Math expression does not follow the infix notation\n')
+            sys.stderr.write('ERROR: Math expression does not follow the infix notation.\n')
             exit(0)
 
         print('Final result: %.3f' % result)
--- src/scripts/homogenise_data.py
+++ src/scripts/homogenise_data.py
@@ -1,6 +1,7 @@
 import os
 import random
 import shutil
+import sys
 from argparse import ArgumentParser
 
 random.seed(42)
@@ -10,20 +11,18 @@ def parse_arguments():
     parser = ArgumentParser(description='Photomath - homogenise the data')
 
     parser.add_argument('--dataset_path', type=str, default='resources/dataset',
-                        help='Path to the characters dataset')
+                        help='path to the characters dataset')
 
     parser.add_argument('--no_of_files', type=int, default=3000,
-                        help='Number of examples for each character')
+                        help='number of examples for each character')
 
     parser.add_argument('--dest', type=str, default='resources/homogenised_dataset',
-                        help='Path for storing the homogenised characters dataset')
+                        help='path to the directory for storing the homogenised characters dataset')
 
     return parser.parse_args()
 
 
-if __name__ == '__main__':
-    args = parse_arguments()
-
+def homogenise_data():
     for subdir, dirs, files in os.walk(args.dataset_path):
         subdir_name = subdir.split('/')[-1]
         if subdir_name == args.dataset_path.split('/')[-1]:
@@ -52,3 +51,15 @@ if __name__ == '__main__':
                     file_name, file_extension = file.split('.')
                     final_file = f'{file_name}_{i}.{file_extension}'
                     shutil.copy(os.path.join(subdir, file), os.path.join(dest, final_file))
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+
+    if not os.path.isdir(args.dataset_path):
+        sys.stderr.write('ERROR: Path to the homogenised characters dataset does not exist.\n')
+        exit(0)
+
+    if not os.path.isdir(args.dest):
+        sys.stderr.write('ERROR: Directory for storing the homogenised characters dataset does not exist.\n')
+        exit(0)
